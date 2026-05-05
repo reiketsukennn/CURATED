03e285cca6a0fa5cbbb18606b98bffcccdd40092
reiketsukennn <95510714+reiketsukennn@users.noreply.github.com>
2026-05-05 21:24:38 +0800

    Delete CURATED directory

--- CURATED/ScrollOffsetKey.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-struct ScrollOffsetKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
-    }
-}
